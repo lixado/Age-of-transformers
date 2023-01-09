217b6968916fecf0d763ed0dbf5af02612bf06b3
Pedro Alves <pmcalves1@gmail.com>
2023-01-09 13:36:34 +0100

    added logger config and requirments txt

---
+++ logger.py
@@ -0,0 +1,22 @@
+import os
+import time
+import shutil
+
+
+class Logger():
+    def __init__(self):
+        # create folder for results
+        self.folder_path = "./results/" + time.strftime("%Y%m%d-%H-%M-%S")
+        os.mkdir(self.folder_path)
+
+        # copy configs
+        shutil.copy2("./config.json", self.folder_path)
+
+        print("Saved to folder " + os.path.abspath(self.folder_path)) 
+
+
+    def save_epoch(self):
+        pass
+
+    def save_model(self):
+        pass
