0f6159738110507573caa8530bb04664abd58374
Pedro Alves <pmcalves1@gmail.com>
2023-01-11 14:18:42 +0100

    organizing code

--- src/logger.py
+++ src/logger.py
@@ -6,7 +6,7 @@ import shutil
 class Logger():
     def __init__(self):
         # create folder for results
-        self.folder_path = "./results/" + time.strftime("%Y%m%d-%H-%M-%S")
+        self.folder_path = "./../results/" + time.strftime("%Y%m%d-%H-%M-%S")
         os.mkdir(self.folder_path)
 
         # copy configs
