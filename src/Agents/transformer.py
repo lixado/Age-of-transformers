b54239f6620b3fe50e272c28c6fea8c3bd7186fc
Andreas Høiberg Eike <andree18@uia.no>
2023-03-08 12:53:03 +0100

    working on transformer

--- src/Agents/transformer.py
+++ src/Agents/transformer.py
@@ -7,17 +7,45 @@ import numpy as np
 import torch
 import math
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, model_dim, k_length):
+        super(PositionalEncoding, self).__init__()
+
+        self.register_buffer('table', self.get_positional_encoding_table(model_dim, k_length))
+
+    def get_positional_encoding_table(self, d_model, k):
+        def get_pos_vector(pos):
+            return [pos / np.power(10000, 2 * (i / d_model)) for i in range(d_model)]
+
+        table = np.array([get_pos_vector(position) for position in range(k)])
+        table[:, 0::2] = np.sin(table[:, 0::2])
+        table[:, 1::2] = np.sin(table[:, 1::2])
+
+        return torch.FloatTensor(table).unsqueeze(0)
+
+
 def ScaledDotProduct(Q, K, V, d):
    qkT = Q @ K.transpose(-2, 1)
    inner = qkT/math.sqrt(d)
-   return nn.Softmax(inner)*V
+   return torch.nn.Softmax(dim=-1)(inner) @ V
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, emb):
+        super().__init__()
         self.linear1 = nn.Linear(emb, 3 * emb)
-        self.scaled_dot_product =
+        self.emb = emb
 
     def forward(self, x):
         B, T, C = x.size()
 
-        query, key, value = self.c_attn(x).split(self.n_embd, dim=2)
+        query, key, value = self.linear1(x).split(self.n_embd, dim=2)
+
+        query = query.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        key = key.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+        value = value.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+        sdp = ScaledDotProduct(query, key, value, self.emb)
+
+class Transformer(nn.Module):
+    def __init__(self):
+        super().__init__()
