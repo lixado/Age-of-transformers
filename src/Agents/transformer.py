6f763230afcc3f4bd0ad7ba2c4d2c55468a91121
Andreas Høiberg Eike <andree18@uia.no>
2023-03-09 23:27:09 +0100

    working on building model with encoder and decoder

--- src/Agents/transformer.py
+++ src/Agents/transformer.py
@@ -1,8 +1,4 @@
-import os
 import torch.nn as nn
-import copy
-from collections import deque
-import random
 import numpy as np
 import torch
 import math
@@ -28,9 +24,13 @@ class PositionalEncoding(nn.Module):
         return torch.FloatTensor(table).unsqueeze(0)
 
 
-def ScaledDotProduct(Q, K, V, d):
+def ScaledDotProduct(Q, K, V, d, mask=None):
    qkT = Q @ K.transpose(-2, 1)
    inner = qkT/math.sqrt(d)
+
+   if mask is not None:
+        inner = inner.masked_fill(mask==0, -1e9)
+
    return torch.nn.Softmax(dim=-1)(inner) @ V
 
 class MultiHeadAttention(nn.Module):
@@ -40,7 +40,7 @@ class MultiHeadAttention(nn.Module):
         self.d_model = d_model
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
 
-    def forward(self, x):
+    def forward(self, x, mask=None):
         B, T, C = x.size()
 
         query, key, value = self.linear1(x).split(self.n_embd, dim=2)
@@ -49,7 +49,10 @@ class MultiHeadAttention(nn.Module):
         key = key.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
         value = value.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
 
-        sdp = ScaledDotProduct(query, key, value, self.d_model)
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+
+        sdp = ScaledDotProduct(query, key, value, self.d_model, mask=mask)
 
         x += sdp # Add
         x = self.layer_norm(x) # Norm
@@ -57,11 +60,70 @@ class MultiHeadAttention(nn.Module):
         return x
 
 class FeedForward(nn.Module):
-    def __init__(self):
+    def __init__(self, d_inner, d_model):
         super().__init__()
+        self.w1 = nn.Linear(d_inner, d_model)
+        self.w2 = nn.Linear(d_inner, d_model)
+        self.activation = nn.ReLU()
+        self.layer_norm = nn.LayerNorm(d_inner, eps=1e-6)
 
+    def forward(self, x):
+        input = self.w1(x)
+        input = self.activation(input)
+        input = self.w2(input)
+
+        x += input # Add
+        x = self.layer_norm(x) #Norm
+
+        return x
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self, d_model, d_inner):
+        super(EncoderLayer).__init__()
+        self.attention = MultiHeadAttention(d_model)
+        self.feed_forward = FeedForward(d_inner, d_model)
+
+    def forward(self, x):
+        x = self.attention(x)
+        x = self.feed_forward(x)
+        return x
+
+class Encoder(nn.Module):
+    def __init__(self, n_layers, d_model, d_inner):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            EncoderLayer(d_model, d_inner) for _ in range(n_layers) # *N
+        ])
+
+    def forward(self):
+        pass
+
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, d_inner):
+        super(DecoderLayer).__init__()
+        self.attention = MultiHeadAttention(d_model)
+        self.encoded_attention = MultiHeadAttention(d_model)
+        self.feed_forward = FeedForward(d_inner, d_model)
+
+    def forward(self, input, output, mask=None):
+        output = self.attention(input, mask=mask)
+        output = self.encoded_attention(output)
+        output = self.feed_forward(output)
+        return output
+
+
+class Decoder(nn.Module):
+    def __init__(self, n_layers, d_model, d_inner):
+        super().__init__()
+        self.layers = nn.ModuleList([
+            DecoderLayer(d_model, d_inner) for _ in range(n_layers)  # *N
+        ])
+
+    def forward(self):
+        pass
 
 
 class Transformer(nn.Module):
-    def __init__(self):
+    def __init__(self, n_layers):
         super().__init__()
