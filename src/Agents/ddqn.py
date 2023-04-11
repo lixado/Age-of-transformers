49da0118dffebf52b8137ba286026e91445dd423
Andreas Høiberg Eike <andree18@uia.no>
2023-04-11 10:23:15 +0200

    changed params

--- src/Agents/ddqn.py
+++ src/Agents/ddqn.py
@@ -18,7 +18,7 @@ class DDQN_Agent:
         self.net = DDQN(self.state_dim, self.action_space_dim).float().to(device=self.device)
 
         self.exploration_rate = 1
-        self.exploration_rate_decay = 0.9999995
+        self.exploration_rate_decay = 0.99999
         self.exploration_rate_min = 0.001
         self.curr_step = 0
         """
@@ -37,7 +37,7 @@ class DDQN_Agent:
             Q learning
         """
         self.gamma = 0.9
-        self.learning_rate = 0.000250
+        self.learning_rate = 0.025
         self.learning_rate_decay = 0.999975
 
         self.optimizer = torch.optim.Adam(self.net.parameters(), lr=self.learning_rate)
@@ -147,7 +147,7 @@ class DDQN_Agent:
         loss.backward()
         self.optimizer.step()
 
-        self.scheduler.step() #
+        #self.scheduler.step() #
 
         return loss.item()
 
