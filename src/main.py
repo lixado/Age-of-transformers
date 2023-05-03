6b35efcad5b842f92244ecb5460cacb58375ca20
Andreas Høiberg Eike <andree18@uia.no>
2023-05-03 14:40:56 +0200

    adjust

--- src/main.py
+++ src/main.py
@@ -91,11 +91,11 @@ if __name__ == "__main__":
     """
     if mode == 0:
         logger = Logger(workingDir)
-        data_path = os.path.join(workingDir, "ddqn_harvest_data_3")
+        data_path = os.path.join(workingDir, "ddqn_harvest")
 
-        train_transformer(config, agent, gym, logger, data_path)
-        #gym = FrameStack(gym, num_stack=FRAME_STACK, lz4_compress=False)
-        #train_ddqn(config, ddqn_agent, gym, logger)
+        #train_transformer(config, agent, gym, logger, data_path)
+        gym = FrameStack(gym, num_stack=FRAME_STACK, lz4_compress=False)
+        train_ddqn(config, ddqn_agent, gym, logger)
     elif mode == 1:
         # get latest model path
         results = os.path.join(workingDir, "results")
