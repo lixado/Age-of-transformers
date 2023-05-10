91515a58f4a16b5020ea9cdcd521f92f6cce447a
Pedro Alves <pedron18@uia.no>
2023-05-10 12:00:26 +0000

    change parameters

--- src/dt_selftrain.py
+++ src/dt_selftrain.py
@@ -85,7 +85,7 @@ def train_dt_self(config: dict, agent: DecisionTransformer_Agent, gym: gym.Env, 
             truncated = False
 
             actionIndex = -1  # first acction is default Do nothing
-            reward = 100
+            reward = 1200
             agent.states_sequence = [observation]
             agent.rewards_sequence = [reward]
             agent.timesteps_sequence = [ticks]
